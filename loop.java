097933d85d769519bac9bf9bcb52b32e15be499d
Aldrin A. Galvez <111488391+drihn@users.noreply.github.com>
2022-10-20 15:43:41 +0800

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,31 @@
+ public class Loop {
+
+        public static void main(String[] args) throws Exception {
+    
+         
+    
+            char[][] Array_3x3 = {
+    
+                {'A', 'B', 'C'},
+    
+                {'D', 'E', 'F'},
+    
+                {'G', 'H', 'I'}
+    
+    };
+    
+            for (int i = 0; i < Array_3x3.length; i++){
+    
+                for (int j = 0; j < Array_3x3.length; j++){
+    
+                        System.out.print(Array_3x3[i][j]);
+    
+                }
+    
+            System.out.print("\n");
+    
+            }
+    
+        }
+    
+    }
