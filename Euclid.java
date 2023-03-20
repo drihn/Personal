2f884dcc74ef0e7b8d1babf6632fca05d1bbbf1a
Aldrin A. Galvez <111488391+drihn@users.noreply.github.com>
2023-03-20 10:15:57 +0800

    Update Euclid.java

--- Euclid.java
+++ Euclid.java
@@ -1,59 +1,88 @@
-package monday;
+package SS;
 
-import java.io.*;
-import java.util.Scanner; // import for scanner
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
 
-class Euclid {
-    private static Scanner sc = new Scanner(System.in); // scanner
-    
-    public static double gcd(double input1, double input2) { // recursion function for Euclid's Algorithm to find the GCD of the user's input
-        if (input2 == 0){ 
-            return input1;
+public class LOLOG extends JFrame implements ActionListener {
+    private ArrayList<String> usernames;
+    private ArrayList<String> passwords;
+    private JLabel usernameLabel;
+    private JLabel passwordLabel;
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+
+    public LOLOG() {
+        super("Login System");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Initialize the usernames and passwords
+        usernames = new ArrayList<String>();
+        passwords = new ArrayList<String>();
+        usernames.add("Admin_01");
+        passwords.add("AdminOne");
+        usernames.add("Admin_02");
+        passwords.add("AdminTwo");
+        usernames.add("Admin_03");
+        passwords.add("AdminThree");
+
+        // Create the login form components
+        usernameLabel = new JLabel("Username:");
+        passwordLabel = new JLabel("Password:");
+        usernameField = new JTextField();
+        passwordField = new JPasswordField();
+        loginButton = new JButton("Login");
+
+        // Set the layout and add the components
+        getContentPane().setLayout(new GridLayout(3, 2));
+        getContentPane().add(usernameLabel);
+        getContentPane().add(usernameField);
+        getContentPane().add(passwordLabel);
+        getContentPane().add(passwordField);
+        getContentPane().add(loginButton);
+
+        // Add the login button action listener
+        loginButton.addActionListener(this);
+
+        // Show the login form
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String username = usernameField.getText();
+        String password = new String(passwordField.getPassword());
+
+        boolean usernameFound = false;
+        boolean passwordMatch = false;
+
+        // Check if the username exists
+        for (int i = 0; i < usernames.size(); i++) {
+            if (usernames.get(i).equals(username)) {
+                usernameFound = true;
+                if (passwords.get(i).equals(password)) {
+                    passwordMatch = true;
+                    break;
+                }
+            }
         }
-        else {
-            return gcd(input2, input1 % input2); // formula for finding the GCD of two integers using Euclid's Algorithm
+
+        // Show the appropriate message
+        if (!usernameFound && !passwordMatch) {
+            JOptionPane.showMessageDialog(this, "Incorrect Username and Password");
+        } else if (!usernameFound) {
+            JOptionPane.showMessageDialog(this, "Incorrect Username");
+        } else if (!passwordMatch) {
+            JOptionPane.showMessageDialog(this, "Incorrect Password");
+        } else {
+            JOptionPane.showMessageDialog(this, "Login Successful!");
         }
     }
 
-    static boolean isPalindrome(int input) {
-        int temp = input;
-        int reverse = 0;
-        while (temp > 0) {
-            int lastDigit = temp % 10;
-            reverse = (reverse * 10) + lastDigit;
-            temp /= 10;
-        }
-        return input == reverse;
+    public static void main(String[] args) {
+        new LOLOG();
     }
-
-    public static void main(String[] args) throws IOException { // main method
-        int input1, input2;  // initializing inputs for the user 
-        double Gcd;
-        
-
-        System.out.println("Enter the 1st number");
-        input1 = sc.nextInt(); 
-        System.out.println("Enter the 2nd number");
-        input2 = sc.nextInt();
-
-        Gcd = gcd(input1, input2);
-
-        FileWriter fw = new FileWriter("tite.txt");
-        PrintWriter print = new PrintWriter(fw);
-
-        if(isPalindrome(input1) == true)
-            print.println(input1 + " is a PALINDROME");
-        else
-            print.println(input1 + " is NOT A PALINDROME");
-
-        if(isPalindrome(input2) == true)
-            print.println(input2 + " is a PALINDROME");
-        else
-            print.println(input2 + " is NOT A PALINDROME");
-
-        print.println("The GCD of these two integers is " + Gcd); // function call.   NOTE : print.println is similar to sout print but print.println will create new file for the output of the final answer.
-        print.println("Suggested Password: " + input1 + "-" + input2 + "-" + Gcd);
-        print.close();
-    }
-    
 }
