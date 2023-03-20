8bc662dae6544d94aca90a67165658d53aa4b12d
Aldrin A. Galvez <111488391+drihn@users.noreply.github.com>
2023-03-20 09:33:58 +0800

    Update main1.java

--- main1.java
+++ main1.java
@@ -1,40 +1,107 @@
-package monday;
+package java;
 
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class main1 {
+public class Adminlog extends JFrame {
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+
+    private ArrayList<User> users;
+
+    public Adminlog() {
+        setTitle("Multiple Login System");
+
+        // Initialize the ArrayList with some default users
+        users = new ArrayList<>();
+        users.add(new User("Admin_01", "AdminOne"));
+        users.add(new User("Admin_02", "AdminTwo"));
+        users.add(new User("Admin_03", "AdminThree"));
     
-    static Scanner sc = new Scanner(System.in);
-    
-     public static int factorial(int input) {
-        if (input > 0) {
-            return input * factorial(input - 1);
-        }
-        else {
-            return 1;
-        }
-     }
 
-     static boolean isPalindrome(int input) {
-        int temp = input;
-        int reverse = 0;
-        while (temp > 0) {
-            int lastDigit = temp % 10;
-            reverse = (reverse * 10) + lastDigit;
-            temp /= 10;
-        }
-        return input == reverse;
+        // Create the login form components
+        JLabel usernameLabel = new JLabel("Username:");
+        JLabel passwordLabel = new JLabel("Password:");
+        usernameField = new JTextField(20);
+        passwordField = new JPasswordField(20);
+        loginButton = new JButton("Login");
+
+        // Add an ActionListener to the login button
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String enteredUsername = usernameField.getText();
+                String enteredPassword = new String(passwordField.getPassword());
+
+                for (User user : users) {
+                    if (user.getUsername().equals(enteredUsername) && user.getPassword().equals(enteredPassword)) {
+                        JOptionPane.showMessageDialog(null, "Login Successful");
+                        // Code to open the main application window
+                        return;
+                    }
+                }
+
+                JOptionPane.showMessageDialog(null, "Invalid username or password");
+            }
+        });
+
+        // Create the login form layout using GroupLayout
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setAutoCreateGaps(true);
+        layout.setAutoCreateContainerGaps(true);
+
+        layout.setHorizontalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(usernameLabel)
+                        .addComponent(passwordLabel))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addComponent(usernameField)
+                        .addComponent(passwordField)
+                        .addComponent(loginButton)));
+
+        layout.setVerticalGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(usernameLabel)
+                        .addComponent(usernameField))
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                        .addComponent(passwordLabel)
+                        .addComponent(passwordField))
+                .addComponent(loginButton));
+
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
-     public static void main(String[] args) {
-        System.out.println("Input a number");
-        int input = sc.nextInt();
-        System.out.println(input + "! is " + factorial(input));
 
-        if (isPalindrome(input) == true) {
-            System.out.println("The factorial is a palindrome.");
-        } else {
-            System.out.println("The factorial is not a palindrome.");
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Adminlog();
+            }
+        });
+    }
+
+    private class User {
+        private String username;
+        private String password;
+
+        public User(String username, String password) {
+            this.username = username;
+            this.password = password;
         }
-        System.out.println("Suggested Password: " + input + "-" + factorial(input));
-    }   
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
 }
